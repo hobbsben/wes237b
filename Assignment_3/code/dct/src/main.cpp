efe5200527d1b5c22ed514556e49ca776d887792
hanrahanp92 <phanraha@ucsd.edu>
2018-07-30 20:53:44 -0700

    commit working cache miss main.cpp

--- Assignment_3/code/dct/src/main.cpp
+++ Assignment_3/code/dct/src/main.cpp
@@ -14,11 +14,12 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, const char * argv[])
-{
+{       
+        unsigned int opencv_cachemisscount;
 	unsigned int c_start;
 	unsigned int opencv_c, student_c;
 
-	cout << "WES237B HW 2" << endl;
+	cout << "WES237B HW 3 " << endl;
 
 	VideoCapture cap("input.raw");
 
@@ -47,7 +48,13 @@ int main(int argc, const char * argv[])
 
 	float avg_perf = 0.f;
 	int count = 0;
-
+        int i_start = 0;
+         //configure register 0x0 to read PMUEVENT[48] (table A.8.1 of ARM Cortex-A9 rev. r4p1, available on piazza)   // from lab 3
+        //  cache_config(0x0, 0x90/*event_number*/);   // from lab3
+        cache_config(0x0, 0x03/*event_number*/);   // (software increment, data cache miss)
+        
+	int cache_start = 0;
+        int cache_start2 = 0; 
 	while(key != 'q')
 	{
 		cap >> frame;
@@ -59,15 +66,22 @@ int main(int argc, const char * argv[])
 
 		init_counters(1, 0);
 
+		cache_start = cache_read(0x0);
 		// OpenCV DCT
-		c_start = get_cyclecount();
+		c_start = get_cyclecount();              // read cycle count 
 		dct(gray, dct_org);
-		opencv_c = get_cyclecount() - c_start;
-
+		opencv_c = get_cyclecount() - c_start;   // get cycle count again and calculate difference
+	        //opencv_cachemisscount = cache_read(0x03);  // call cash miss count reading function from cycletime.h
+               // cout << "\n OpenCV method Cache Misses: " << opencv_cachemisscount/*- i_start */ <<
+		cout << "OpenCV Cache Miss Count: " <<  cache_read(0x0) - cache_start /*- i_start*/ << endl;
+                 
+		cache_start2 = cache_read(0x0);
 		// Your DCT
 		LinuxTimer t;
 		c_start = get_cyclecount();
-		dct_student = student_dct(gray);
+		dct_student = student_dct(gray);              
+		cout << "Our DCT Cache Miss Count: " <<  cache_read(0x0) - cache_start2 << "\n" /*- i_start*/ << endl;
+		
 		student_c = get_cyclecount() - c_start;
 		t.stop();
 		float myTimer = t.getElapsed();
@@ -84,7 +98,7 @@ int main(int argc, const char * argv[])
 		
 		count++;
 
-		
+		//cout << opencv_cachemisscount << "   \n"  << endl;  //print opencv cache misses
 		cout << opencv_c << "  " << student_c << "  "
 			<< (double)myTimer/1000000000.0 << endl;
 
@@ -92,6 +106,7 @@ int main(int argc, const char * argv[])
 		avg_perf += perf;
 		printf("Performance: %.3f%% (avg: %.3f%%), RMSE: %.4f\n", perf, avg_perf/count, sqrt(mse));
 
+                // opencv_cachemisscount = 0;  // set opencv cache miss count to 0
 
 		Mat inverse;
 		idct(dct_student, inverse);
@@ -101,8 +116,12 @@ int main(int argc, const char * argv[])
 		imshow("IDCT Output", inverse);
 		moveWindow("IDCT Output", 500, 0);
 		key = waitKey(10);
+                
 	}
 
+	// cout << "Cache Misses: " << pmn_read(0x0) - i_start << endl;
+	// cout << "Cache Misses: " << cache_read(0x0) /*- i_start */ << endl;
+        		
 	return 0;
 }
 
