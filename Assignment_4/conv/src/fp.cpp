f9d5596aec24ebc0def623208b9db2e1d03d3e17
hanrahanp92 <phanraha@ucsd.edu>
2018-08-17 16:54:08 -0700

    re-arrange directory structure according to assignment specification

---
+++ Assignment_4/conv/src/fp.cpp
@@ -0,0 +1,124 @@
+#include "main.h"
+#include "cycletime.h"
+
+using namespace std;
+using namespace cv;
+
+#define FADD(a,b)   ((a)+(b))
+#define FMUL(a,b,q) (((a)*(b))>>(q))
+#define FCONV(a, q1, q2) (((q2)>(q1)) ? (a)<<((q2)-(q1)) : (a)>>((q1)-(q2)))
+#define TOFIX(d, q) ((int)( (d)*(double)(1<<(q)) ))
+#define TOFLT(a, q) ( (double)(a) / (double)(1<<(q)) )
+
+int main(int argc, const char *argv[]) {
+
+	int len = 1024;
+	float arr_cos_flt[len];
+	int arr_cos_fix[len];
+	
+	int freq = 512;
+	Mat image_flt, image_fix, result_flt, result_fix;
+	image_flt = Mat(200, len+88, CV_32F);
+	image_fix = Mat(200, len+88, CV_8U);
+
+        cout << "WES237B Assignment4_p_2 - fixpoint" << endl;
+_get:
+	cout << "enter read and write exponents: q1 q2" << endl;
+
+	int q1, q2;
+	scanf("%d %d", &q1, &q2);
+	
+	if(q1>14 || q2>14){
+	  cout << "exponent must be less or equal to 14" << endl;
+	  goto _get;
+	}	
+
+	for (int j=0; j<len; j++){
+	  arr_cos_flt[j] = cos( 2.0*(float)((j)%freq)/(float)(freq) * M_PI);
+	 // cout<<100+50*arr_cos_flt[j]<<endl;
+	  arr_cos_fix[j] = TOFIX(arr_cos_flt[j], q1);
+	}
+	
+	for(int j=0; j<len; j++){//len=1024
+	  image_flt.at<float>(100+50*arr_cos_flt[j], j+44) = 1;
+	  image_fix.at<uchar>(100+50*TOFLT(FCONV(arr_cos_fix[j], q1, q2), q2), j+44) = 255;
+	}
+
+	/* drawing signals */
+	imshow("float", image_flt);
+	cvMoveWindow("float", 20,0); //changed this for VM
+	imshow("fix", image_fix);
+	cvMoveWindow("fix", 20,400);// changed this for VM
+	waitKey();
+	
+	int klen = 5;
+	float kernel_flt[] = {0.0625, 0.125, 0.25, 0.5, 0.75};	
+	int kernel_fix[klen];
+	for(int j=0; j<klen; j++){
+	  kernel_fix[j] = TOFIX(kernel_flt[j], q1);
+	}
+	float res_flt[len+klen-1];
+	int res_fix[len+klen-1];
+
+	for(int j=0; j<len+klen-1; j++){
+	  res_flt[j]=0;
+	  res_fix[j]=0;
+	}
+
+/* DO NOT MODIFY ANYTHING ABOVE THIS LINE */
+	
+	/*1D convolution using float */
+        unsigned int c_start;
+        init_counters(1, 0);
+        c_start = get_cyclecount();
+	/******************** insert your code here ********************/
+	for(int i=0; i<len+klen-1;i++)
+	{
+		for(int j=0;j<klen;j++)
+		{
+		res_flt[i]+=arr_cos_flt[i]*kernel_flt[j];
+		}
+}
+	/* ********************************************************* */
+        cout << "float CPU Cycles: " << get_cyclecount() - c_start << endl;
+	
+	/*1D convolution using FIXED POINT */
+        init_counters(1, 0);
+        c_start = get_cyclecount();
+	/* ******************** insert your code here ******************** */
+	for(int i=0; i<len+klen-1;i+=1)
+	{
+		for(int j=0;j<klen;j++)
+		{
+	res_fix[i]+=arr_cos_fix[i]*kernel_fix[j];
+		}
+	}
+	/* *********************************************************** */
+        cout << "fixpoint CPU Cycles: " << get_cyclecount() - c_start << endl;
+
+/* DO NOT MODIFY ANYTHING BELOW THIS LINE */
+
+	/* calculating RMSE */
+        double _se = 0;
+	for(int j=0; j<len; j++){
+	  float _d = (abs(res_flt[j] - TOFLT(FCONV(res_fix[j], 2*q1, q2), q2)));
+	  _se += _d*_d;
+	}
+	double _mse = _se/(float)len;
+	double _rmse = sqrt(_mse);
+
+        cout << "RMSE: " << _rmse << endl;
+	
+	/* drawing results */
+	for(int j=0; j<len; j++){
+	  image_flt.at<float>(100+50*res_flt[j], j+44) = 1;
+	  image_fix.at<uchar>(100+50*TOFLT(FCONV(res_fix[j], 2*q1, q2), q2), j+44) = 255;
+	}
+	imshow("float", image_flt);
+	cvMoveWindow("float", 20,0); //changed for VM
+	imshow("fix", image_fix);
+	cvMoveWindow("fix", 20,400); // changed for VM
+	waitKey();
+ 
+  return 0;
+}
