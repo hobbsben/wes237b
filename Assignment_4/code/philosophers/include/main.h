37b5efc810b370826afd68ca9f695332c7c4ea47
hanrahanp92 <phanraha@ucsd.edu>
2018-08-17 13:43:08 -0700

    commiting latest philosophers

---
+++ Assignment_4/code/philosophers/include/main.h
@@ -0,0 +1,22 @@
+//
+//  main.h
+//  Lab2
+//
+//  Created by Alireza on 7/6/16.
+//  Copyright © 2016 Alireza. All rights reserved.
+//
+
+#ifndef main_h
+#define main_h
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#endif /* main_h */
