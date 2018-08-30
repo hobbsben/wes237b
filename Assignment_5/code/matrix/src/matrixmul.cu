19882318559ddc5352add9b38b3f9f8cc93fc755
Pat <phanraha@ucsd.edu>
2018-08-29 22:27:17 -0700

    intital push after gdog

--- Assignment_5/code/matrix/src/matrixmul.cu
+++ Assignment_5/code/matrix/src/matrixmul.cu
@@ -13,7 +13,7 @@ __global__ void block_mm_kernel(const float* A, const float* B, float* output, i
 	int tidx = threadIdx.x;
         int tidy = threadIdx.y;
         float sum = 0;
-;        
+        
 
         const int NUM_BLOCKS = M/(BLOCK_SIZE);  // calculate amount of blocks
         
@@ -22,27 +22,34 @@ __global__ void block_mm_kernel(const float* A, const float* B, float* output, i
         //__shared__  float C_shared[BLOCK_SIZE*BLOCK_SIZE];
 
         //loop through blocks in the grid 
-        for(int grid_block = 0; grid_block < NUM_BLOCKS; grid_block++)       
+        for(int grid_block = 0; grid_block < 1; grid_block++)       
         {   
             //check grid boundary conditions
             //if(tidx+tidy*grid_block < M*N)
             //{
                 //store input matrices into shared memory
-                A_shared[tidx+tidy*BLOCK_SIZE] = A[(grid_block*BLOCK_SIZE)+tidx+(tidy+gidy)*M];
-                B_shared[tidx+tidy*BLOCK_SIZE] = B[gidx+tidx+(grid_block*BLOCK_SIZE+tidy)*M];
+                A_shared[tidx+(tidy*BLOCK_SIZE)] = A[(tidx + BLOCK_SIZE* grid_block) +(gidy*M/*added M*/)];
+                B_shared[tidx+(tidy*BLOCK_SIZE)] = B[gidx*N + (tidy+BLOCK_SIZE*  grid_block)];
+
+
                 __syncthreads();
              //}
          // loop through elements within the block
-         for(int block_element = 0; block_element < BLOCK_SIZE; block_element++)
-            {
-               // check block boundary conditions
-	       //  if(tidx+tidy*+grid_block*BLOCK_SIZE < M*N)
-	       // {
-	           sum += A_shared[tidx+tidy*BLOCK_SIZE] * B_shared[tidx+tidy*BLOCK_SIZE + block_element];
-	       // }
-           }
-           output[gidx+gidy*N ] += sum;
+         //for(int a_row = 0; a_row < BLOCK_SIZE; a_row++)
+          //  {
+            for(int b_row = 0; b_row < BLOCK_SIZE; b_row++)
+                {
+                
+                   sum += A_shared[tidx  + (b_row  * BLOCK_SIZE)] 
+                        * B_shared[tidx  + (b_row  * BLOCK_SIZE)];
+	           
+                 
+	        }
+          // }
+           output[gidx+(gidy*N) ] = sum;
         }
+//printf("%d a_shared : %f\n",blockIdx.x, A_shared[tidx+(tidy*BLOCK_SIZE)]);
+//printf("%d b_shared : %f\n",blockIdx.y, B_shared[tidx+(tidy*BLOCK_SIZE)]);
         
         
 }//endline
